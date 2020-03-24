49d0f14167db6a45058638efdc4a24a139edfa01
Rohanda <s-rohanda.abdallah@zewailcity.edu.eg>
2020-03-24 16:05:48 -0700

    -Asking the user to enter the year -Looping on all top 50 movies in this year and printing them

--- IMBD_top50_scraper.py
+++ IMBD_top50_scraper.py
@@ -1,18 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.imdb.com/search/title/?year=2019&title_type=feature&"
+# Ask the user to enter the year used to get top ratings
+year = input("Please enter the year you want to get top ratings(e.g: 2016): ")
+
+# Build the url
+url = "https://www.imdb.com/search/title/?year="+year+"&title_type=feature&"
 Response = requests.get(url)
+
+#Use Beautiful Soup library to find movie titles in the response
 Soup = BeautifulSoup(Response.text, 'html.parser')
-Re =  Soup.find_all(attrs={"class": "lister-item-index unbold text-primary"})
-print(Re[49].next_sibling.next_sibling.string)
-#TODO : 1- loop on all titles ,extract them and save them in a json file
-#       2-Extract the following info:
-#       "1": {
-#         "name": "Torpedo Boat, 'Dupont'",
-#         "certificate": "",
-#         "runtime": "",
-#         "genre": "",
-#         "description": "\"A stunning view of one of the fastest boats in Uncle Sam's torpedo fleet. She is running under forced draught at full headway, and comes like a race horse directly toward the camera, ...                See full summary\u00a0\u00bb",
-#         "director": "",
-#         "stars": []
+articles =  Soup.find_all("div",attrs = {"class":"article"})
+Main_header = articles[0].h1.string
+print(Main_header)
+Sub_articles = articles[0].find_all("div",attrs ={"class":"lister-item mode-advanced"})
+i = 0
+y =[]
+
+# Write all movie titles
+for article in Sub_articles :
+    y.append({i+1:{"name":article.h3.a.string}})
+    print (y[i])
+    i = i+1
+
